2018d8b6fe25831895b3510b1822cead5d4fedd9
shmibblez <shmibblez@gmail.com>
2021-02-16 20:42:23 -0500

    currently not working, tried a bunch of stuff but still nothing. I'm gonna try and make some changes and take a closer look, hopefully I can get this working

--- src/test.js
+++
@@ -1,8 +0,0 @@
-// const bindings = require("../build/Release/geocomb.node");
-
-// console.log("creating icosahedron");
-// const ico = new bindings.IcosahedronWrapper();
-// console.log("creating point from coords");
-// const p = ico.pointFromCoords(71, 27);
-// console.log("generating hash");
-// console.log(JSON.stringify(ico.hash(p, 10), null, 2));
